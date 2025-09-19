140ee4cb68aac47c69d28896749f283ac6eb5cb6
Minh Anh Nguyen <minhanhn@usf.edu>
2025-09-19 13:20:56 -0400

    Creeated first array of objects js file

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+const product = [
+    {name:"Lipstick", category: "makeup", price: 40.50, inventory: 50},
+    {name: "Mascara", category: "makeup", price: 30, inventory: 100},
+    {name: "Laptop", category: "electronics", price: 1000, inventory: 150},
+    {name: "Carpet", category: "household", price: 100, inventory: 3},
+    {name: "Jacket", category: "apparel", price: 200, inventory: 200}
+];
